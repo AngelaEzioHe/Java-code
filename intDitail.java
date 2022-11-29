abfad53185b4a0f9a24c147484d95904ac5a1624
瑶妹的狗 <114608263+hugenut@users.noreply.github.com>
2022-11-29 17:43:34 +0800

    Add files via upload

---
+++ intDitail.java
@@ -0,0 +1,10 @@
+public class intDitail
+{
+    public static void main(String[] args)
+    {
+        int n1 = 1;
+        long n2 = 1L;
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+}
