4ae7a166a356e2b6a0a42188fc29422dc3fdf87d
瑶妹的狗 <114608263+hugenut@users.noreply.github.com>
2022-11-29 17:44:57 +0800

    Add files via upload

---
+++ FloatDetail.java
@@ -0,0 +1,10 @@
+public class FloatDetail
+{
+    public static void main(String[] args)
+    {
+        double  f1 = 1.1;
+        float   f2 = 1.2F;
+        System.out.println(f1);
+        System.out.println(f2);
+    }
+}
