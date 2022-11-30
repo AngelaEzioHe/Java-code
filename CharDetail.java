b9af89c7fbc549f221dfd3a1ba9b667d42ac0055
瑶妹的狗 <114608263+hugenut@users.noreply.github.com>
2022-11-30 10:34:31 +0800

    Add files via upload

---
+++ CharDetail.java
@@ -0,0 +1,11 @@
+public class CharDetail
+{
+    public static void main(String[] args)
+    {
+        char c1 ='何';
+        System.out.println((int)c1);
+        char c2 = 20309;
+        System.out.println(c2);
+        System.out.println('a' + 10);
+    }
+}
