65d3429be5697241490e1099c1301b256a5efe9e
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2022-12-13 00:25:45 +0800

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class NestedIf
+{
+    public static void main(String[] args)
+    {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("请输入该歌手的成绩");
+        double score = myScanner.nextDouble();
+        if(score > 8.0)
+        {
+            System.out.println("请输入姓别：");
+            char gender = myScanner.next().charAt(0);
+            if(gender == '男')
+            {
+                System.out.println("进入男子组");
+            }
+            else if(gender == '女')
+            {
+                System.out.println("进入女子组");
+            }
+            else
+            {
+                System.out.println("性别有误");
+            }
+        }
+        else
+        {
+            System.out.println("sorry你被淘汰了");
+        }
+    }
+}
