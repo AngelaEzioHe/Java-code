d099e7583449e86cc5968b2e4ae051295d67dc14
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2022-12-25 00:19:22 +0800

    Add files via upload

--- Method01.java
+++ Method01.java
@@ -1,10 +1,18 @@
 public class Method01 {
     public static void main(String[] args) {
-
+        /*
+            1.方法写好后，如果不去调用(使用)，不会输出。
+            2.先创建对象，然后调用方法
+         */
+        person2 p1=new person2();
+        p1.speak(); //调用方法
+        p1.cal01();
+        p1.cal02(5);
+        System.out.println("两数和="+p1.add(2,3));
     }
 }
 
-class  person{
+class  person2{
     String name;
     int age;
     /*
@@ -17,4 +25,22 @@ class  person{
     public void speak(){
         System.out.println("我是一个好人");
     }
+    public void cal01(){
+        int result=0;
+        for(int i=1;i<=1000;i++){
+            result+=i;
+        }
+        System.out.println("cal01计算结果="+result);
+    }
+    public void cal02(int n){
+        int result=0;
+        for(int i=1;i<=n;i++){
+            result+=i;
+        }
+        System.out.println("cal02计算结果="+result);
+     }
+     public int add(int num1,int num2){
+        int result=num1+num2;
+        return result;
+     }
 }
