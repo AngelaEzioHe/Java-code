bcb5ad890cc253cfc4dff577c0788064094ab74e
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2022-12-24 23:36:37 +0800

    Add files via upload

---
+++ Method01.java
@@ -0,0 +1,20 @@
+public class Method01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class  person{
+    String name;
+    int age;
+    /*
+        1.public 表示方法是公开的
+        2.void 表示没有返回值
+        3.speak 是方法名
+        4.() 是形参列表
+        5.{} 是方法体，可以写要执行的代码
+     */
+    public void speak(){
+        System.out.println("我是一个好人");
+    }
+}
---
+++ Object02.java
@@ -0,0 +1,15 @@
+public class Object02 {
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.age=10;
+        p1.name="小明";
+        Person p2=p1;
+        p1.age=80;
+        System.out.println(p2.age);
+    }
+}
+
+class Person{
+    String name;
+    int age;
+}
