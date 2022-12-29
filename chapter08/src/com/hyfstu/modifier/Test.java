42c1253051acc4564a6f7460edb1e25972f3bc37
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2022-12-29 14:20:32 +0800

    Add files via upload

---
+++ chapter08/src/com/hyfstu/modifier/Test.java
@@ -0,0 +1,13 @@
+package com.hyfstu.modifier;
+
+public class Test {
+    public static void main(String[] args) {
+        A a=new A();
+        a.m1();
+        B b=new B();
+        b.say();
+    }
+}
+ class Tiger{
+
+}
---
+++ chapter08/src/com/hyfstu/pkg/Test.java
@@ -0,0 +1,10 @@
+package com.hyfstu.pkg;
+
+import com.hyfstu.modifier.A;
+
+public class Test {
+    public static void main(String[] args) {
+        A a=new A();
+        System.out.println(a.n1);
+    }
+}
