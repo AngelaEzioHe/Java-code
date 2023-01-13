1aa666ec9bc6aba79ecfa7a7853c361e06d609df
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2023-01-13 17:19:20 +0800

    Add files via upload

---
+++ hyfstu/extend_/exercise/ExtendsExercise.java
@@ -0,0 +1,8 @@
+package hyfstu.extend_.exercise;
+
+public class ExtendsExercise {
+    public static void main(String[] args) {
+        PC pc = new PC("intel",16,512,"IBM");
+        pc.printInfo();
+    }
+}
