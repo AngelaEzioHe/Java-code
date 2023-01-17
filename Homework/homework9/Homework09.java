206a817211f1654926c613ca97ce30e378b5593c
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2023-01-17 20:11:07 +0800

    Delete Homework directory

--- Homework/homework9/Homework09.java
+++
@@ -1,8 +0,0 @@
-package hyfstu.Homework.homework9;
-
-public class Homework09 {
-    public static void main(String[] args) {
-        LabeledPoint black = new LabeledPoint("Black", 1929, 230.07);
-
-    }
-}
