890a243023b4e1828b281341661bfc49124cdc28
遇见神鹿 <114608263+hugenut@users.noreply.github.com>
2023-04-13 12:40:25 +0800

    Add files via upload

---
+++ chapter12/src/exception_/NullPointerException_.java
@@ -0,0 +1,12 @@
+package exception_;
+
+/**
+ * @Author: EzioHe
+ * @Date: 2023/4/13 0:05
+ */
+public class NullPointerException_ {
+    public static void main(String[] args) {
+        String name=null;
+        System.out.println(name.length());
+    }
+}
